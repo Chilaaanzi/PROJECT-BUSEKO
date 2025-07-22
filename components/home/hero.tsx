3bad74a62f480d7329973fe58892ee15d40f0fc0
David Chilanzi Sensenta <90356316+Chilanzi-thirt33n@users.noreply.github.com>
2025-07-22 14:46:08 +0200

    hero 80dvh

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -46,7 +46,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="relative bg-zinc-900 min-h-screen lg:min-h-[80vh] flex items-center"
+      className="relative bg-zinc-900 min-h-screen lg:min-h-[80dvh] flex items-center"
     >
       {/* Background */}
       <div className="absolute inset-0 overflow-hidden">
